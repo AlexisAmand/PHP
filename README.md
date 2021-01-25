47bd3557ba88e479e4ddf2eebf534c8e8f8de778
alexisamand <alexis.amand@gmail.com>
2021-01-25 15:11:24 +0100

    Tri à Bulles

---
+++ README.md
@@ -0,0 +1,8 @@
+### QBasic
+
+Just a few PHP scripts.
+
+### List of Algorithms
+
+* **Tris** : Some sorting algorithms : 
+	* bubble sort (in french we say "Tri à Bulles") 
---
+++ tri-bulles.php
@@ -0,0 +1,41 @@
+<?php
+
+/* Taille du tableau */
+
+$n = 10;
+
+/* Pour la demo, on met des valeurs aleatoires dans le tableau */
+
+echo "Tableau avant le tri :<br />";
+echo "<table border='1'><tr>";
+for ($i = 0; $i < $n; $i++)
+    {
+    $tablo[$i] = rand(0,100);
+    echo "<td>".$tablo[$i]."</td>";
+    }
+echo "</tr></table>";
+
+/* on fait le tri */
+
+for ($i = $n - 1; $i >= 0;$i--)
+    {
+    for ($j = 0; $j <= $i - 1; $j++)
+        {
+        if($tablo[$j] > $tablo[$j+1])
+            {
+            $temp = $tablo[$j + 1];  
+            $tablo[$j + 1] = $tablo[$j];
+            $tablo[$j] = $temp;
+            }
+        }
+    }
+
+/* On affiche le tableau final */
+
+echo "Tableau avec le tri :<br />";
+echo "<table border='1'><tr>";
+for ($i = 0; $i < $n; $i++)
+    {
+    echo "<td>".$tablo[$i]."</td>";
+    }
+echo "</tr></table>";
